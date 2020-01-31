7e187b9341dce19236df4d496fcfa3734ef6e091
ismcodifiquer <ismaelvi1601@icloud.com>
2020-01-31 16:14:31 +0100

    baraja modificada con test de funciones y repartir

--- baraja.py
+++ baraja.py
@@ -1,4 +1,5 @@
 import random
+
 _palos = ['o', 'c', 'e', 'b']
 _cartas = ['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
 
@@ -24,4 +25,39 @@ def mezclar(b):
         b[i] = b[al_azar]
         b[al_azar] = aux
     return b
-            
+
+class Baraja():
+    naipes = []
+    __palos = ['o', 'c', 'e', 'b']
+    __cartas = ['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
+
+    def __init__(self):
+        self.naipes = []
+        for palo in _palos:
+            for carta in self.__cartas:
+                self.naipes.append(carta + palo)
+
+    def elige_carta(self, i):
+        return random.randint(0, len(self.naipes)-1)
+    
+    def mezclar(self):
+        for i in range(len(self.naipes)):
+            # Elegimos carta de intercambio al azar
+            al_azar = self.elige_carta(i)
+
+            # Cambiamos la carta por orden (i) por la carta elegida al azar
+            aux = self.naipes[i]
+            self.naipes[i] = self.naipes[al_azar]
+            self.naipes[al_azar] = aux
+
+    def repartir(self, mano, jugadores):
+        res = []
+        for j in range(jugadores):
+            res.append([])
+
+        for c in range(mano):
+            for item in res:
+                carta = self.naipes.pop(0)
+                item.append(carta)
+
+        return res
--- baraja_tests.py
+++ baraja_tests.py
@@ -2,13 +2,18 @@ import unittest
 from unittest.mock import patch
 import baraja
 
-def mi_elige_carta(i, longitud):
+def mi_eligecarta(i, longitud):
     if i == longitud-1:
         return 0
     else:
         return i+1
 
-class BarajaTest(unittest.TestCase):
+def class_eligecarta(self, i):
+    return mi_eligecarta(i, len(self.naipes))
+
+
+
+class BarajaFuncionalTest(unittest.TestCase):
 
     def test_crear_baraja(self):
         b = baraja.baraja()
@@ -19,12 +24,56 @@ class BarajaTest(unittest.TestCase):
         self.assertEqual(b[10], 'Ac')
         self.assertEqual(b[20], 'Ae')
 
-    @patch('baraja.elige_carta', mi_elige_carta)
+    @patch('baraja.elige_carta', mi_eligecarta)
     def test_mezclar_lista(self):
         b = ['A', 'B', 'C', 'D', 'E']
         mezclada = ['A', 'C', 'D', 'E', 'B']
         b = baraja.mezclar(b)
         self.assertEqual(b, mezclada)
+
+
+
+class BarajaObjetoTest(unittest.TestCase):
+    def test_crear_baraja(self):
+        b = baraja.Baraja()
+        self.assertEqual(len(b.naipes), 40) 
+    
+        self.assertEqual(b.naipes[0], 'Ao')
+        self.assertEqual(b.naipes[39], 'Rb')
+        self.assertEqual(b.naipes[10], 'Ac')
+        self.assertEqual(b.naipes[20], 'Ae')
+
+
+    @patch('baraja.Baraja.elige_carta', class_eligecarta)
+    def test_mezclar_lista(self):
+        b = baraja.Baraja()     
+        b.mezclar()
+
+
+        self.assertEqual(b.naipes[0], 'Ao')
+        self.assertEqual(b.naipes[39], '2o')
+        self.assertEqual(b.naipes[10], '2c')
+        self.assertEqual(b.naipes[20], '2e')
+
+    def test_mezclar_sin_comprobar_orden(self):
+        b = baraja.Baraja()
+        b.mezclar()
+
+        self.assertEqual(len(b.naipes), 40)
+
+    def test_repartir_baraja(self):
+        b = baraja.Baraja()
+
+        jugadas = b.repartir(3,2)
+        self.assertEqual(len(jugadas), 2)
+        self.assertEqual(len(b.naipes), 34)
+        self.assertEqual(jugadas[0], ['Ao', '3o', '5o'])
+        self.assertEqual(jugadas[1], ['2o', '4o', '6o'])
+        
+
+
+    
+
 
     
 if __name__ == '__main__':
